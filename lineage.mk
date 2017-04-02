ef04d629eb204f2a2abc5351ccf41c911a85dd9b
Anderson Mendes <lge500meu@gmail.com>
2017-04-01 21:27:04 -0300

    PN PLZ

--- lineage.mk
+++
@@ -1,9 +0,0 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-# Inherit some common DU stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
-
-PRODUCT_RELEASE_NAME := falcon
-PRODUCT_NAME := du_falcon
-
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
